4326429d9f0f1b98e27411b5c912721a265f5dea
Himanshu Singh <himanshuhappy002@gmail.com>
2023-07-02 07:29:59 +0530

    initial commit

---
+++ MaxMinElement.java
@@ -0,0 +1,14 @@
+class MaxMinElement{
+public static void main(String[] args) {
+    int[] arr = {1,8,9,2,4,0};
+    int min = arr[0];
+    int max = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+        min = Math.min(min, arr[i]);
+        max = Math.max(max, arr[i]);
+    }
+
+    System.out.println(min);
+    System.out.println(max);
+}
+}
